fbba686cd755401a4ddad81dd5b8f8eacd794bf4
harshitasingh28 <harshitaaa2807@gmail.com>
2026-08-18 12:17:10 +0530

    Time: 1764 ms (13.16%), Space: 9.4 MB (76.1%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -10,23 +10,19 @@ public:
         }
         return true;
     }
-    int f(int i, string& s, vector<int>& dp){
-        if(i == s.length())return 0;
-       // string temp = "";
-        int mini = INT_MAX;
-        if(dp[i] != -1)return dp[i];
-        for(int j = i;j<s.length();j++){
-               //temp += s[j];
-               if(isPalindome(i, j, s)){
-                int cost = 1 + f(j+1, s, dp);
-                 mini = min(mini, cost);
-               }
-        }
-        return dp[i] = mini;
-    }
     int minCut(string s) {
         int l = s.length();
-        vector<int> dp(l, -1);
-        return f(0, s, dp) - 1;
+        vector<int> dp(l+1, 0);
+        for(int i = l-1;i>=0;i--){
+            int mini = INT_MAX;
+            for(int j = i;j<l;j++){
+                if(isPalindome(i, j, s)){
+                int cost = 1 + dp[j+1];
+                mini = min(mini, cost);
+                dp[i] = mini;
+                }
+            }
+        }
+        return dp[0]-1;
     }
 };
